357f7d65a7e101e0077a47b6b5ec6839477de978
bcgosselin <bcgosselin@gmail.com>
2024-04-23 18:11:07 -0500

    added comments and cleaned up console logs used for debug

--- index.js
+++ index.js
@@ -1,31 +1,40 @@
+// declaration to hold imported inquirer module
 let inquirer;
 
+// import
 import('inquirer').then((inquirerModule) => {
+  // assignment
   inquirer = inquirerModule.default;
   
+  // required modules
   const fs = require('fs');
   const path = require('path');
   const { generateSVG } = require('./lib/utils');
 
+  // prompt user for input
   inquirer
     .prompt([
+      // prompt for logo text
       {
         type: 'input',
         name: 'text',
         message: 'Enter up to three characters for the logo text:',
         validate: input => (input.length > 0 && input.length <= 3 ? true : 'Please enter up to three characters.'),
       },
+      // prompt for text color
       {
         type: 'input',
         name: 'textColor',
         message: 'Enter text color (color keyword or hexadecimal number):',
       },
+      // prompt for shape
       {
         type: 'list',
         name: 'shape',
         message: 'Choose a shape:',
         choices: ['triangle', 'circle', 'square'],
       },
+      // prompt for shape color
       {
         type: 'input',
         name: 'shapeColor',
@@ -33,18 +42,19 @@ import('inquirer').then((inquirerModule) => {
       },
     ])
     .then(answers => {
+      // Deconstruct answers to get input values
       const { text, textColor, shape, shapeColor } = answers;
-      console.log('User input:', { text, textColor, shape, shapeColor });
-
+      
+      // Generate SVG string based on input
       const svgString = generateSVG(text, textColor, shape, shapeColor);
 
+      // write file to logo.svg file in root directory
       fs.writeFile('logo.svg', svgString, err => {
         if (err) {
           console.error('Error writing file:', err);
           return;
         }
         console.log('Generated logo.svg');
-        console.log('File saved at:', path.resolve('logo.svg'));
       });
     })
     .catch(err => {
